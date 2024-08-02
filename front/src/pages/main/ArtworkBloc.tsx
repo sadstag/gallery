1c24f1ae83a2a26de24e5163e4c2fa4cf89694fb
Fréderic Sarron <frederic.sarron@gmail.com>
2024-08-02 21:43:58 +0200

    fix(biome)

--- front/src/pages/main/ArtworkBloc.tsx
+++ front/src/pages/main/ArtworkBloc.tsx
@@ -1,26 +1,29 @@
-import { useArtworksDBResource } from '../../context/ArtworksDBProvider'
-import { Artwork } from '../../model/Artwork'
-import styles from './Wall.module.css'
+import styles from "./Wall.module.css";
+
+import { useArtworksDBResource } from "../../context/ArtworksDBProvider";
+import type { Artwork } from "../../model/Artwork";
+
 
 type Props = {
-    artwork: Artwork
+	artwork: Artwork;
 }
+
 export function ArtworkBloc(props: Props) {
-    const {
-        artwork: { id, title }
-    } = props
+	const {
+		artwork: { id, title },
+	} = props;
 
-    const artworksDB = useArtworksDBResource()
+	const artworksDB = useArtworksDBResource();
 
-    const style = {
-        'background-image': `url(${artworksDB?.()?.getImageURL(id, 'small')})`
-    }
+	const style = {
+		'background-image': `url(${artworksDB?.()?.getImageURL(id, "small")})`,
+	}
 
-    return (
-        <a href={`/artwork/${id}`}>
-            <div class={styles.artwork} style={style}>
-                <div class={styles.title}>{title}</div>
-            </div>
-        </a>
-    )
+	return (
+		<a href={`/artwork/${id}`}>
+			<div class={styles.artwork} style={style}>
+				<div class={styles.title}>{title}</div>
+			</div>
+		</a>
+	);
 }
--- front/src/pages/main/Wall.tsx
+++ front/src/pages/main/Wall.tsx
@@ -1,18 +1,29 @@
-import { For } from 'solid-js'
-import { useArtworksDBResource } from '../../context/ArtworksDBProvider'
-import { ArtworkBloc } from './ArtworkBloc'
-import styles from './Wall.module.css'
+import { useBeforeLeave, useLocation } from "@solidjs/router";
+import { For, createEffect } from "solid-js";
+import { useArtworksDBResource } from "../../context/ArtworksDBProvider";
+import { ArtworkBloc } from "./ArtworkBloc";
+import styles from "./Wall.module.css";
+
+window.history.scrollRestoration = "manual";
 
 export function Wall() {
-    const artworksDB = useArtworksDBResource()
+	const artworksDB = useArtworksDBResource()
 
-    return (
-        <div class={styles.wall}>
-            <For each={artworksDB?.()?.artworks ?? []}>
-                {(artwork) => {
-                    return <ArtworkBloc artwork={artwork} />
-                }}
-            </For>
-        </div>
-    )
+	const location = useLocation<{ scrollPosition: number }>()
+
+	createEffect(() => {
+		window.scroll(0, location.state?.scrollPosition ?? 0)
+	})
+
+	useBeforeLeave(() => {
+		window.history.replaceState({ scrollPosition: window.scrollY }, "")
+	})
+
+	return (
+		<div class={styles.wall}>
+			<For each={artworksDB?.()?.artworks ?? []}>
+				{artwork => <ArtworkBloc artwork={artwork} />}
+			</For>
+		</div>
+	);
 }
